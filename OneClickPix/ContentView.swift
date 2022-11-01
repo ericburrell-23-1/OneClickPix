a10739f0c36b31a3e23bbf299d5ff464878193f5
Eric Burrell <ericburrell231@gmail.com>
2022-10-31 20:35:07 -0500

    Initial Commit

--- OneClickPix/ContentView.swift
+++ OneClickPix/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,6 +22,7 @@ struct ContentView: View {
     }
 }
 
+    // MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
