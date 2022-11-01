ec0335f4aa88d289dc752830056a3dddd65a6bbb
Eric Burrell <ericburrell231@gmail.com>
2022-10-31 20:24:06 -0500

    Initial Commit

---
+++ OneClickPix/OneClickPixApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneClickPixApp.swift
+//  OneClickPix
+//
+//  Created by Eric Burrell on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct OneClickPixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
